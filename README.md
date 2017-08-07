826507f165ad27ce4d500491b499fcc4f253bde7
rummykhan <rehan_manzoor@outlook.com>
2017-08-08 00:53:56 +0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+blog
+====
+
+A Symfony project created on August 7, 2017, 11:28 pm.
---
+++ src/AppBundle/Entity/Post.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity()
+ * @ORM\Table(name="posts")
+ */
+class Post
+{
+    /**
+     * @var int
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     * @ORM\Column(type="string", length="255")
+     */
+    private $title;
+
+    /**
+     * @var string
+     * @ORM\Column(type="string", length="500")
+     */
+    private $slug;
+
+    /**
+     * @var string
+     * @ORM\Column(type="text")
+     */
+    private $body;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * @param string $title
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+    }
+
+    /**
+     * @return string
+     */
+    public function getSlug()
+    {
+        return $this->slug;
+    }
+
+    /**
+     * @param string $slug
+     */
+    public function setSlug($slug)
+    {
+        $this->slug = $slug;
+    }
+
+    /**
+     * @return string
+     */
+    public function getBody()
+    {
+        return $this->body;
+    }
+
+    /**
+     * @param string $body
+     */
+    public function setBody($body)
+    {
+        $this->body = $body;
+    }
+}
+
