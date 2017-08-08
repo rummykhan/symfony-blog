d95f4e415cb246c4458cadb84891b92d093cf3c3
rummykhan <rehan_manzoor@outlook.com>
2017-08-09 02:19:40 +0400

    Minor updates

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -5,6 +5,7 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
@@ -13,7 +14,17 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
+        $templating = $this->container->get('templating');
+
         // replace this example code with whatever you need
         return $this->render('frontend/default/home/index.html.twig');
+    }
+
+    /**
+     * @Route("/tmp/{name}", defaults={"name"= null})
+     */
+    public function testAction($name=null)
+    {
+        return new Response($name);
     }
 }
